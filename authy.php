4e7eee54a43a7fa49243cf76067b548a26773013
Johanna Mantilla Duque <johanna1431@gmail.com>
2013-05-10 15:49:00 -0500

    Update version to 2.2

--- authy.php
+++ authy.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/authy/authy-wordpress
  * Description: Add <a href="http://www.authy.com/">Authy</a> two-factor authentication to WordPress.
  * Author: Authy Inc
- * Version: 2.1
+ * Version: 2.2
  * Author URI: https://www.authy.com
  * License: GPL2+
  * Text Domain: authy
